4907d9d696c0980033577fe1ea4bf7552664d545
prodriguezdefino <prodriguezdefino@gmail.com>
2021-07-19 18:40:30 -0700

    improved batching parallelism for file compose, now there is a way to define number of files per window but not size per composed file

--- launch_datagen_templates.sh
+++ launch_datagen_templates.sh
@@ -5,14 +5,15 @@ SCHEMA_FILENAME=$2
 SCHEMA_LOCATION=$3$SCHEMA_FILENAME
 QPS=$4
 TOPIC=$5
-REGIONS=( "${@:6}" )
+PREFIX_NAME=$6
+REGIONS=( "${@:7}" )
 
 echo "Copying the local schema file to the GCS location..."
 gsutil cp $SCHEMA_FILENAME $SCHEMA_LOCATION
 
 for REGION in "${REGIONS[@]}";
   do
-    gcloud beta dataflow flex-template run pubsub-json-datagen-$REGION --project=$PROJECT --region=$REGION --template-file-gcs-location=gs://dataflow-templates/latest/flex/Streaming_Data_Generator --parameters schemaLocation=$SCHEMA_LOCATION,qps=$QPS,topic=$TOPIC,maxNumWorkers=50,enableStreamingEngine=true
+    gcloud beta dataflow flex-template run $PREFIX_NAME-pubsub-json-datagen-$REGION --project=$PROJECT --region=$REGION --template-file-gcs-location=gs://dataflow-templates/latest/flex/Streaming_Data_Generator --parameters schemaLocation=$SCHEMA_LOCATION,qps=$QPS,topic=$TOPIC,maxNumWorkers=50,enableStreamingEngine=true
   done
 
 echo ""
