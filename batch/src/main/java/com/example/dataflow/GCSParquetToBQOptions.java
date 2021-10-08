d2f297d69f155e7fb645cf9becab075b828ab025
prodriguezdefino <prodriguezdefino@gmail.com>
2021-10-07 22:31:43 -0700

    renamed batch project, added GCS to BQ pipeline

---
+++ batch/src/main/java/com/example/dataflow/GCSParquetToBQOptions.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.example.dataflow;
+
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.Validation;
+import org.apache.beam.sdk.options.ValueProvider;
+
+/**
+ * Options supported by the pipeline.
+ *
+ * <p>
+ * Inherits standard configuration options.
+ */
+public interface GCSParquetToBQOptions extends DataflowPipelineOptions {
+
+  @Description("Input location for the AVRO files.")
+  @Validation.Required
+  ValueProvider<String> getInputLocation();
+
+  void setInputLocation(ValueProvider<String> value);
+
+  @Description("Local path to the AVRO schema to use.")
+  @Validation.Required
+  String getAvroSchemaFileLocation();
+
+  void setAvroSchemaFileLocation(String value);
+
+  @Description("Output BQ table FQN <project:dataset.table>")
+  @Validation.Required
+  ValueProvider<String> getOutputTableSpec();
+
+  void setOutputTableSpec(ValueProvider<String> value);
+
+  @Description("Includes an insert timestamp column in the table and populates with the insertion time.")
+  @Default.Boolean(true)
+  Boolean isIncludeInsertTimestamp();
+
+  void setIncludeInsertTimestamp(Boolean value);
+  
+  @Description("Inserts data using BQ batch loads.")
+  @Default.Boolean(true)
+  Boolean isBatchUpload();
+
+  void setBatchUpload(Boolean value);
+}
---
+++ batch/src/main/java/com/example/dataflow/GCSParquetToBigQuery.java
@@ -0,0 +1,313 @@
+/*
+ * Copyright (C) 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.example.dataflow;
+
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
+import com.google.api.services.bigquery.model.TimePartitioning;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.avro.JsonProperties;
+import org.apache.avro.LogicalTypes;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.coders.AvroGenericCoder;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryUtils;
+import org.apache.beam.sdk.io.gcp.bigquery.InsertRetryPolicy;
+import org.apache.beam.sdk.io.gcp.bigquery.WriteResult;
+import org.apache.beam.sdk.io.parquet.ParquetIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.schemas.utils.AvroUtils;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This pipeline ingests incoming data from a Cloud Pub/Sub topic and outputs the raw data into windowed Avro files at the specified output
+ * directory.
+ *
+ * <p>
+ * Output files will have the following schema:
+ *
+ * <pre>
+ *   {
+ *      "type": "record",
+ *      "name": "Event"
+ *      "namespace": "com.example.dataflow",
+ *      "fields": [
+ *          {"name": "id", "type": "string"},"
+ *          {"name": "isActive", "type": "boolean"},"
+ *          {"name": "balance", "type": "double"},"
+ *          {"name": "picture", "type": "string"},"
+ *          {"name": "age", "type": "long"},"
+ *          {"name": "eyeColor", "type": "string"},"
+ *          {"name": "name", "type": "string"},"
+ *          {"name": "gender", "type": "string"},"
+ *          {"name": "company", "type": "string"},"
+ *          {"name": "email", "type": "string"},"
+ *          {"name": "phone", "type": "string"},"
+ *          {"name": "address", "type": "string"},"
+ *          {"name": "registered", "type": "long"},"
+ *          {"name": "latitude", "type": "double"},"
+ *          {"name": "longitude", "type": "double"},"
+ *          {"name": "tags", "type": {"type": "array", "items": "string"}},"
+ *          {"name": "timestamp", "type": "long"},"
+ *          {"name": "about", "type": "string"},"
+ *          {"name": "about2", "type": "string"},"
+ *          {"name": "about3", "type": "string"},"
+ *          {"name": "about4", "type": "string"},"
+ *          {"name": "about5", "type": "string"}"
+ *      ]
+ *   }
+ * </pre>
+ *
+ * <p>
+ * Example Usage:
+ *
+ * <pre>
+ * mvn compile exec:java -Dexec.mainClass=com.example.dataflow.PubsubToBigQuery -Dexec.cleanupDaemonThreads=false -Dexec.args=" \
+ * --project=$PROJECT \
+ * --avroSchemaFileLocation='avro-schema.json' \
+ * --stagingLocation=gs://$STAGING_BUCKET/dataflow/staging \
+ * --tempLocation=gs://$STAGING_BUCKET/dataflow/temp \
+ * --numWorkers=50 \
+ * --maxNumWorkers=99 \
+ * --runner=DataflowRunner \
+ * --workerMachineType=n1-highmem-2 \
+ * --usePublicIps=false \
+ * --region=us-central1 \
+ * --numShards=70 \
+ * --inputLocation=gs://$BUCKET/parquet/2021/10/04/17/*.parquet \
+ * --outputLocation=gs://$BUCKET/csv/ \
+ * --outputTableSpec=$BQ_TABLE \
+ * --jobName='gcs-avro-to-csv' "
+ * </pre>
+ */
+public class GCSParquetToBigQuery {
+
+  private static final Logger LOG = LoggerFactory.getLogger(GCSParquetToBigQuery.class);
+
+  private static final String BQ_INSERT_TIMESTAMP_FIELDNAME = "insertionTimestamp";
+
+  /**
+   * Main entry point for executing the pipeline.
+   *
+   * @param args The command-line arguments to the pipeline.
+   * @throws java.io.IOException
+   */
+  public static void main(String[] args) throws IOException {
+
+    GCSParquetToBQOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(GCSParquetToBQOptions.class);
+
+    run(options);
+  }
+
+  /**
+   * Runs the pipeline with the supplied options.
+   *
+   * @param options The execution parameters to the pipeline.
+   * @return The result of the pipeline execution.
+   */
+  static PipelineResult run(GCSParquetToBQOptions options) throws IOException {
+
+    // Create the pipeline
+    Pipeline pipeline = Pipeline.create(options);
+
+    // read AVRO schema from local filesystem
+    final String avroSchemaStr = Files.readAllLines(Paths.get(options.getAvroSchemaFileLocation()))
+            .stream()
+            .collect(Collectors.joining("\n"));
+
+    LOG.info("Pipeline will be using AVRO schema:\n{}", avroSchemaStr);
+
+    final Schema avroSchema = new Schema.Parser().parse(avroSchemaStr);
+    final org.apache.beam.sdk.schemas.Schema beamSchema = AvroUtils.toBeamSchema(avroSchema);
+
+    TableSchema bqSchema = BigQueryUtils.toTableSchema(beamSchema);
+    SerializableFunction<GenericRecord, TableRow> bqFormatFunction
+            = record -> BigQueryUtils.toTableRow(AvroUtils.toBeamRowStrict(record, null));
+
+    if (options.isIncludeInsertTimestamp()) {
+      bqSchema
+              = bqSchema.set(BQ_INSERT_TIMESTAMP_FIELDNAME,
+                      new TableFieldSchema()
+                              .setName(BQ_INSERT_TIMESTAMP_FIELDNAME)
+                              .setType("TIMESTAMP")
+                              .setMode("NULLABLE"));
+
+      bqFormatFunction = record -> {
+        Row beamRow = AvroUtils.toBeamRowStrict(record, null);
+        TableRow tableRow = BigQueryUtils
+                .toTableRow(beamRow)
+                .set(BQ_INSERT_TIMESTAMP_FIELDNAME, "AUTO");
+        return tableRow;
+      };
+    }
+
+    /*
+     * Steps:
+     *   1) Read messages from PubSub
+     *   2) Parse those JSON message into AVRO
+     *   3) Insert into BQ using Streaming API
+     *   4) If configured, add an insert timestamp into the BQ table
+     *   5) If configured, when the column does not exists, update the BQ table schema to add the column
+     */
+    PCollection<GenericRecord> records = pipeline
+            .apply("MatchParquetFiles", FileIO.match().filepattern(options.getInputLocation()))
+            .apply("ReadMatches", FileIO.readMatches())
+            .apply("ReadGenericRecords", ParquetIO.readFiles(avroSchema));
+
+    if (options.isBatchUpload()) {
+      Schema timestampedSchema = addNullableTimestampFieldToAvroSchema(avroSchema, BQ_INSERT_TIMESTAMP_FIELDNAME);
+      String timestampedSchemaStr = timestampedSchema.toString();
+      records
+              .apply("AddTSToRecord", ParDo.of(new AddInsertTimestampToGenericRecord(timestampedSchemaStr)))
+              .setCoder(AvroGenericCoder.of(timestampedSchema))
+              .apply("WriteToBQ",
+                      BigQueryIO.<GenericRecord>write()
+                              .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
+                              .withSchemaUpdateOptions(
+                                      Sets.newHashSet(
+                                              BigQueryIO.Write.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+                                              BigQueryIO.Write.SchemaUpdateOption.ALLOW_FIELD_RELAXATION))
+                              .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                              .withMethod(BigQueryIO.Write.Method.FILE_LOADS)
+                              .withTimePartitioning(
+                                      new TimePartitioning()
+                                              .setField(BQ_INSERT_TIMESTAMP_FIELDNAME)
+                                              .setType("DAY"))
+                              .to(options.getOutputTableSpec() + "$20211007")
+                              // bq schema based on the new avro schema
+                              .withSchema(
+                                      BigQueryUtils.toTableSchema(
+                                              AvroUtils.toBeamSchema(new Schema.Parser().parse(timestampedSchemaStr))))
+                              // new avro schema, string format because is not serializable
+                              .withAvroSchemaFactory(bqschema -> new Schema.Parser().parse(timestampedSchemaStr))
+                              // simple writer configuration, not much to do
+                              .withAvroWriter(
+                                      avroWriteRequest -> avroWriteRequest.getElement(),
+                                      schema -> new GenericDatumWriter<>(schema)));
+
+    } else {
+      WriteResult bqWriteResults
+              = records
+                      .apply("WriteToBQ",
+                              BigQueryIO.<GenericRecord>write()
+                                      .skipInvalidRows()
+                                      .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                                      .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
+                                      .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                                      .to(options.getOutputTableSpec())
+                                      .withSchema(bqSchema)
+                                      .withFormatFunction(bqFormatFunction)
+                                      .withExtendedErrorInfo()
+                                      .withAutoSharding()
+                                      .withFailedInsertRetryPolicy(InsertRetryPolicy.retryTransientErrors()));
+
+    }
+    // Execute the pipeline and return the result.
+    return pipeline.run();
+  }
+
+  static class AddInsertTimestampToGenericRecord extends DoFn<GenericRecord, GenericRecord> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AddInsertTimestampToGenericRecord.class);
+
+    private final String readerSchemaStr;
+    private Schema readerSchema;
+
+    public AddInsertTimestampToGenericRecord(String readerSchemaStr) {
+      this.readerSchemaStr = readerSchemaStr;
+    }
+
+    @Setup
+    public void setupBundle() {
+      readerSchema = new Schema.Parser().parse(readerSchemaStr);
+    }
+
+    @ProcessElement
+    public void processElement(ProcessContext context) throws IOException {
+      GenericRecord record = context.element();
+      Schema writerSchema = record.getSchema();
+      try {
+        GenericDatumWriter<GenericRecord> w = new GenericDatumWriter<>(writerSchema);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
+
+        w.write(record, encoder);
+        encoder.flush();
+
+        DatumReader<GenericRecord> reader = new GenericDatumReader<>(writerSchema, readerSchema);
+        BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(outputStream.toByteArray(), null);
+        GenericRecord result = reader.read(null, decoder);
+
+        // adding current local timestamp as microseconds, supported on BQ for avro batch loads
+        result.put(BQ_INSERT_TIMESTAMP_FIELDNAME, OffsetDateTime.now().minusDays(1).toEpochSecond() * 1000 * 1000);
+
+        context.output(result);
+      } catch (IOException e) {
+        LOG.warn("Error while trying to add timestamp to generic record {} with schema {}", record, writerSchema);
+        LOG.error("Error while adding timestamp to record", e);
+      }
+    }
+
+  }
+
+  static Schema addNullableTimestampFieldToAvroSchema(Schema base, String fieldName) {
+    Schema timestampMilliType
+            = Schema.createUnion(
+                    Lists.newArrayList(
+                            Schema.create(Schema.Type.NULL),
+                            LogicalTypes.timestampMillis().addToSchema(Schema.create(Schema.Type.LONG))));
+
+    List<Schema.Field> baseFields = base.getFields().stream()
+            .map(field -> new Schema.Field(field.name(), field.schema(), field.doc(), field.defaultVal()))
+            .collect(Collectors.toList());
+
+    baseFields.add(new Schema.Field(fieldName, timestampMilliType, null, JsonProperties.NULL_VALUE));
+
+    return Schema.createRecord(
+            base.getName(),
+            base.getDoc(),
+            base.getNamespace(),
+            false,
+            baseFields);
+  }
+
+}
